6352b37838da08da8e6288a667ffaf877517d5ec
aman1011019 <aman1011019>
2025-08-09 07:01:17 +0530

    first commit

--- src/MacContainer.jsx
+++ src/MacContainer.jsx
@@ -2,10 +2,14 @@ import { useGLTF, useScroll, useTexture } from '@react-three/drei';
 import { useFrame } from '@react-three/fiber';
 import React from 'react'
 import * as THREE from "three";
+import bgImage from './video/ganpati clg.mp4'
 
 const MacContainer = () => {
+    <video autoPlay loop muted>
+        <source src={bgImage} type="video/mp4"/>
+    </video>
     let model = useGLTF("./ma.glb");
-    let tex = useTexture("/gan.jpg");
+    let tex = useTexture("/gan.jpg ");
     let meshes = {};
     model.scene.traverse(e => {
         meshes[e.name] = e;
